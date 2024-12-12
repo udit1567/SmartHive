3519757e9016e3ef2457588b9f01c9f1f82fe0de
Udit Maurya <119149306+udit1567@users.noreply.github.com>
2024-12-12 23:56:48 +0530

    gg

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import FlickeringGrid from './components/FlickeringGrid';
 function App() {
   return (
     <div>
-      <FlickeringGrid width={1220} height={600} squareSize={4} gridGap={6} flickerChance={0.1} maxOpacity={0.5} color="rgb(0, 0, 0)" />
+      <FlickeringGrid width={1210} height={560} squareSize={4} gridGap={6} flickerChance={0.5} maxOpacity={0.4} color="rgb(4, 59, 65)" />
     </div>
   );
 }
