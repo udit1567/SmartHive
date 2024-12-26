38db8e818039de3751759ff487b86f6ce85d041e
Vishu <vishusrivastava0415@gmail.com>
2024-12-26 22:34:31 +0530

    black

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -188,7 +188,7 @@ export default function Navbar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
+      <AppBar position="fixed"  sx={{ backgroundColor: 'black' }}>
         <Toolbar>
           <IconButton
             size="large"
