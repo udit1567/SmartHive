84958a144f8a1d7bb4bca06511c0a2353ff2979a
vishutx <vishusrivastava0415@gmail.com>
2025-08-27 15:53:12 +0530

    alignmnet corrected

--- frontend/src/Components/PlantMonitoring.js
+++ frontend/src/Components/PlantMonitoring.js
@@ -5,42 +5,47 @@ import MoistureCard from "./MoistureCard";
 import MoistureGraph from "./MoistureGraph";
 import Sidenav from "../Dashboard/Sidenav";
 import PreviewImage from "./PreviewImage";
-#drgfthfh
+
 const PlantMonitoring = () => {
   return (
     <>
       <Box height={100} />
+
+      {/* Layout wrapper */}
       <Box sx={{ display: "flex" }}>
-        <Sidenav />
-        {/* Adjust spacing for navbar & sidenav */}
+        {/* Sidebar */}
+        <Box sx={{ width: 240, flexShrink: 0 }}>
+          <Sidenav />
+        </Box>
 
-        <Container maxWidth="lg">
-          <Grid container spacing={2}>
-            {/* Top Moisture Indicator */}
-            <Grid item xs={12} md={6} lg={2.5}>
-              <MoistureCard />
+        {/* Main content */}
+        <Box sx={{ flexGrow: 1, p: 2 }}>
+          <Container maxWidth="xl">
+            <Grid container spacing={2}>
+              {/* Top Moisture Indicator */}
+              <Grid item xs={12} md={4} lg={3}>
+                <MoistureCard />
+              </Grid>
+
+              {/* Moisture Graph */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Box sx={{ height: 400, width: "100%" }}>
+                  <MoistureGraph />
+                </Box>
+              </Grid>
+
+              {/* Image Upload Component */}
+              <Grid item xs={12} md={6}>
+                <ImageUpload />
+              </Grid>
+
+              {/* Printing previous images */}
+              <Grid item xs={12} md={6}>
+                <PreviewImage apiUrl="http://127.0.0.1:5000/fetch_plant_disease_images" />
+              </Grid>
             </Grid>
-
-            {/* Moisture Graph please no errors*/}
-            <Grid item xs={12} md={6} lg={9} sx={{ width: "500px", height: "400px" }}>
-              <MoistureGraph />
-            </Grid>
-
-            {/* Image Upload Component */}
-            <Grid item xs={12} md={6} lg={6}>
-              <ImageUpload />
-            </Grid>
-
-            {/*Printing previous images*/}
-            <Grid item xs={12} md={6} lg={6} >
-              
-              <PreviewImage apiUrl="http://127.0.0.1:5000/fetch_plant_disease_images" />
-
-            </Grid>
-
-            
-          </Grid>
-        </Container>
+          </Container>
+        </Box>
       </Box>
     </>
   );
